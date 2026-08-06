9b101e433294fd5cb32ea89ce46548cdb2b8530f
Satoru Sarwesh <nareshkumarkushwaha382@gmail.com>
2026-08-06 07:41:58 +0530

    Initialize Firebase with modular SDK
    Added Firebase initialization with modular SDK v10/v12.

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,22 @@
+/**
+ * @file firebase-config.js
+ * @description Firebase initialization module using modular SDK v10+/v12+
+ */
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
+
+const firebaseConfig = {
+    apiKey: "YOUR_FIREBASE_API_KEY",
+    authDomain: "your-app.firebaseapp.com",
+    databaseURL: "https://your-app-default-rtdb.firebaseio.com",
+    projectId: "your-app",
+    storageBucket: "your-app.appspot.com",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getDatabase(app);
