7fac945ff674a7d26a2aa06090fb6b6e1fd1cd6d
Satoru Sarwesh <nareshkumarkushwaha382@gmail.com>
2026-08-11 06:51:59 +0530

    Update Supabase URL in configuration

--- supabase-config.js
+++ supabase-config.js
@@ -5,7 +5,7 @@
 
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const SUPABASE_URL = "https://hatzbtpcdgduyucdjsad.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://hatzbtpcdgduyucdjsad.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhhdHpidHBjZGdkdXl1Y2Rqc2FkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYyMjg5NzIsImV4cCI6MjEwMTgwNDk3Mn0.LBIreXvBKRdkILsNFS4utuhmwrzUhxnKK3gdjvFyFE4";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
